828b5cf9d5665ad77546efe32bf9bf1ddc3b6763
Radu Iulian <iulianradu21@outlook.com>
2025-11-27 21:50:33 +0200

    small bitset rework and style

--- include/DataTypes/BitSet.h
+++ include/DataTypes/BitSet.h
@@ -1,37 +1,38 @@
 #ifndef BIT_SET_H
 #define BIT_SET_H
-#include "utils.h"
 #include <cstdint>
 #include <vector>
 
+#include "utils.hpp"
 namespace mc
 {
-    class BitSet
+    class bit_set
     {
     public:
-        BitSet() = default;
-        inline BitSet(int size) : m_data((size / (sizeof(long) * 8 )) + 1, 0){}
+        constexpr bit_set() noexcept = default;
+        constexpr bit_set(int size) noexcept : m_data((size / (sizeof(long) * 8 )) + 1, 0){}
+        constexpr ~bit_set() = default;
 
-        inline bool Test(int index)const
+        constexpr bool test(int index)const
         {
             return (m_data[index / 64] & (1 << ( index % 64))) != 0;
         }
-        inline void Set(int index, bool value)
+        constexpr void set(int index, bool value)
         {
-            m_data[index / 64] = m_data[index / 64] | (1 << (index % 64)); 
+            m_data[index / 64] = m_data[index / 64] | (1 << (index % 64));
         }
     private:
-        friend struct iu::Serializer<mc::BitSet>;
+        friend struct iu::Serializer<mc::bit_set>;
         std::vector<long> m_data;
     };
 
-    using BitSetSerializer = struct iu::Serializer<mc::BitSet>;
+    using BitSetSerializer = struct iu::Serializer<mc::bit_set>;
 }
 
 template<>
-struct iu::Serializer<mc::BitSet>
+struct iu::Serializer<mc::bit_set>
 {
-    inline void Serialize(std::vector<uint8_t>& buffer, const mc::BitSet& object)
+    inline void Serialize(std::vector<uint8_t>& buffer, const mc::bit_set& object)
     {
         mc::util::writeVarInt(buffer, object.m_data.size());
         iu::Serializer<decltype(object.m_data)>().Serialize(buffer, object.m_data);
